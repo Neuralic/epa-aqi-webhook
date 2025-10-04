3944cebb03e65f5f056b19ec02de6f5a63aa1df2
Neuralic <oluwaseyidavid2005@gmail.com>
2025-10-04 18:13:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 // In-memory storage for GPS results (expires after 30 minutes)
